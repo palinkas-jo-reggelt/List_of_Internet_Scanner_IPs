8a74d685859d44998476ed02049c98d28f890e58
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2025-11-18 10:00:58 +0100

    Update UpdateScannerFWRule.ps1
    Roll FW rules into 10k increments, the maximum allowed by Win Defender firewall

--- UpdateScannerFWRule.ps1
+++ UpdateScannerFWRule.ps1
@@ -1,9 +1,15 @@
 $AddressArray = (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/palinkas-jo-reggelt/List_of_Internet_Scanner_IPs/refs/heads/main/Scanner_IP_ranges.txt').Content.Split([Environment]::NewLine) | Where-Object {$_.Trim() -ne ''}
 
-Try {
-	Get-NetFirewallRule -DisplayName 'Internet_Scanners_Blocklist' -ErrorAction Stop | Out-Null
-	Set-NetFirewallRule -DisplayName 'Internet_Scanners_Blocklist' -RemoteAddress $AddressArray
-}	
-Catch {
-	New-NetFirewallRule -DisplayName 'Internet_Scanners_Blocklist' -Direction Inbound -Action Block -RemoteAddress $AddressArray | Out-Null
+# Create or update firewall rules with ranges found - max 10k per rule
+$ChunkSize = 10000
+For ($i = 0; $i -lt $AddressArray.Count; $i += $ChunkSize) {
+	$Chunk = $AddressArray[$i..($i + $ChunkSize - 1)]
+	$RuleName = "Internet_Scanners_Blocklist-$($i/$ChunkSize)"
+	Try {
+		Get-NetFirewallRule -DisplayName $RuleName -ErrorAction Stop | Out-Null
+		Set-NetFirewallRule -DisplayName $RuleName -RemoteAddress $Chunk
+	}	
+	Catch {
+		New-NetFirewallRule -DisplayName $RuleName -Direction Inbound -Action Block -RemoteAddress $Chunk | Out-Null
+	}
 }
